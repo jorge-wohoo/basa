3476fc0e9284cf82ad275838319008b5f06a7d20
Jorge Estrada <jorge@wohoo.tech>
2022-04-30 15:47:13 -0600

    [UPD] l10ngt adendas fix anulaciones

--- adenddas_guatemala/models/account_move.py
+++ adenddas_guatemala/models/account_move.py
@@ -20,33 +20,37 @@ class AccountMove(models.Model):
     def generate_dte_xml(self, dte):
         annulled = isinstance(dte, AnulacionDTE)
         xml_str = dte_to_xml_annulled(dte) if annulled else dte_to_xml(dte)
-        tag_adendda = ("<dte:Adenda>" '\n'
-                    "<Agente>%(Agente)s</Agente>" '\n'
-                    "<Vencimiento>%(Vencimiento)s</Vencimiento>" '\n'
-                    "<NumeroInterno>%(NumeroInterno)s</NumeroInterno>" '\n'
-                    "</dte:Adenda>"
-        ) % {"Agente": self.invoice_user_id.name, "Vencimiento": self.invoice_date_due, "NumeroInterno": self.name}
-        index_final = xml_str.find("</dte:DTE>")
-        dte_end = xml_str.find("</dte:SAT>")
-        if self.invoice_payment_term_id:
-            index_adendda_tag = tag_adendda.find("</dte:Adenda>")
-            tag_adendda_payment_term = ("<CondicionesPago>%(CondicionesPago)s</CondicionesPago>" '\n') % {"CondicionesPago": self.invoice_payment_term_id.name}
-            tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_payment_term + tag_adendda[index_adendda_tag:]
-        if self.partner_id.ref:
-            index_adendda_tag = tag_adendda.find("</dte:Adenda>")
-            tag_adendda_codigo_cliente = ("<CodigoCliente>%(CodigoCliente)s</CodigoCliente>" '\n') % {"CodigoCliente": self.partner_id.ref}
-            tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_codigo_cliente + tag_adendda[index_adendda_tag:]
-        if self.sale_order_id:
-            index_adendda_tag = tag_adendda.find("</dte:Adenda>")
-            tag_adendda_sale_order = ("<NoOCCliente>%(NoOcCliente)s</NoOCCliente>" '\n'
-                                    "<FechaPedido>%(FechaPedido)s</FechaPedido>" '\n'
-                                    "<NoPedido>%(NoPedido)s</NoPedido>" '\n'
-            ) % {"NoOcCliente": self.sale_order_id.name, "FechaPedido": self.sale_order_id.date_order, "NoPedido": self.sale_order_id.name}
-            tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_sale_order + tag_adendda[index_adendda_tag:]
-        if self.transport:
-            index_adendda_tag = tag_adendda.find("</dte:Adenda>")
-            tag_adendda_transporte = ("<Transporte>%(Transporte)s</Transporte>" '\n') % {"Transporte": self.transport}
-            tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_transporte + tag_adendda[index_adendda_tag:]
-        xml_str = xml_str[:index_final + 10] + tag_adendda + xml_str[dte_end:]
-        fname = self.get_fname_xml(annulled)
-        self.generate_attachment_from_xml_string(xml_str, fname)
+        if not annulled:
+            tag_adendda = ("<dte:Adenda>" '\n'
+                        "<Agente>%(Agente)s</Agente>" '\n'
+                        "<Vencimiento>%(Vencimiento)s</Vencimiento>" '\n'
+                        "<NumeroInterno>%(NumeroInterno)s</NumeroInterno>" '\n'
+                        "</dte:Adenda>"
+            ) % {"Agente": self.invoice_user_id.name, "Vencimiento": self.invoice_date_due, "NumeroInterno": self.name}
+            index_final = xml_str.find("</dte:DTE>")
+            dte_end = xml_str.find("</dte:SAT>")
+            if self.invoice_payment_term_id:
+                index_adendda_tag = tag_adendda.find("</dte:Adenda>")
+                tag_adendda_payment_term = ("<CondicionesPago>%(CondicionesPago)s</CondicionesPago>" '\n') % {"CondicionesPago": self.invoice_payment_term_id.name}
+                tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_payment_term + tag_adendda[index_adendda_tag:]
+            if self.partner_id.ref:
+                index_adendda_tag = tag_adendda.find("</dte:Adenda>")
+                tag_adendda_codigo_cliente = ("<CodigoCliente>%(CodigoCliente)s</CodigoCliente>" '\n') % {"CodigoCliente": self.partner_id.ref}
+                tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_codigo_cliente + tag_adendda[index_adendda_tag:]
+            if self.sale_order_id:
+                index_adendda_tag = tag_adendda.find("</dte:Adenda>")
+                tag_adendda_sale_order = ("<NoOCCliente>%(NoOcCliente)s</NoOCCliente>" '\n'
+                                        "<FechaPedido>%(FechaPedido)s</FechaPedido>" '\n'
+                                        "<NoPedido>%(NoPedido)s</NoPedido>" '\n'
+                ) % {"NoOcCliente": self.sale_order_id.name, "FechaPedido": self.sale_order_id.date_order, "NoPedido": self.sale_order_id.name}
+                tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_sale_order + tag_adendda[index_adendda_tag:]
+            if self.transport:
+                index_adendda_tag = tag_adendda.find("</dte:Adenda>")
+                tag_adendda_transporte = ("<Transporte>%(Transporte)s</Transporte>" '\n') % {"Transporte": self.transport}
+                tag_adendda = tag_adendda[:index_adendda_tag] + tag_adendda_transporte + tag_adendda[index_adendda_tag:]
+            xml_str = xml_str[:index_final + 10] + tag_adendda + xml_str[dte_end:]
+            fname = self.get_fname_xml(annulled)
+            self.generate_attachment_from_xml_string(xml_str, fname)
+        else:
+            fname = self.get_fname_xml(annulled)
+            self.generate_attachment_from_xml_string(xml_str, fname)
